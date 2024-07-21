698e05efc10c3f90e84a92b974eebd55ccb9ba26
Luis Fernando Arevalo <LuisFerArevalo@Luiss-MacBook-Pro.local>
2024-07-21 17:58:28 -0500

    encontrando el erorr

--- index.js
+++ index.js
@@ -2,24 +2,26 @@ import express from 'express'
 import bodyParser from 'body-parser'; 
 import cookieParser from 'cookie-parser';
 import { router } from './routes/main.js'
+// import { SECRET_JWT_KEY } from './config.js';
 
 const app = express()
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(cookieParser())
-app.use((req, res, next) => {
-  const token = req.cookies.access_token
-  req.session = { user: null}
 
-  try {
-    const data = jwt.verify(token, SECRET_JWT_KEY)
-    req.session.user = data
-  } catch {}
+// MIDDLEWARE TOKEN
+// app.use((req, res, next) => {
+//   const token = req.cookies.access_token
+//   req.session = { user: null}
 
-  next()
-  
-})
+//   try {
+//     const data = jwt.verify(token, SECRET_JWT_KEY)
+//     req.session.user = data
+//   } catch {}
+
+//   next()
+// })
 
 app.set('port', process.env.PORT || 3000)
 app.set('view engine', 'ejs')
