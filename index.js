50922875f5af4cdf7d0e1ce11e1bb9bc5e5c3620
Luis Fernando Arevalo <LuisFerArevalo@Luiss-MacBook-Pro.local>
2024-08-01 08:50:15 -0500

    path views

--- index.js
+++ index.js
@@ -3,10 +3,15 @@ import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import bodyParser from 'body-parser'; 
 import cookieParser from 'cookie-parser';
+import path from 'path'
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
 import { router } from './routes/main.js'
 // import { SECRET_JWT_KEY } from './config.js';
 
 const app = express()
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 dotenv.config({ path: './config.env' })
 
@@ -35,6 +40,7 @@ app.use((req, res, next) => {
 
 app.set('port', process.env.PORT || 3000)
 app.set('view engine', 'ejs')
+app.set('views', path.join(__dirname, './views'))
 
 // Routes
 app.use('/', router)
